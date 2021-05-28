b4b5c46e3f6bf21fefacca0293226d1d883d15db
aziz <aob102@gmail.com>
2021-05-28 09:17:28 +0200

    global selectors added

--- src/main/java/ResultPage.java
+++ src/main/java/ResultPage.java
@@ -3,14 +3,14 @@ import org.openqa.selenium.WebDriver;
 
 public class ResultPage {
     WebDriver driver;
-
+By ProductElement = By.cssSelector("[data-uuid='s-searchgrid-carousel:0']");
     public ResultPage(WebDriver driver) {
         this.driver = driver;
     }
 
     public ProductPage chooseProduct() {
 
-        driver.findElement(By.cssSelector("[data-uuid='s-searchgrid-carousel:0']")).click();
+        driver.findElement(ProductElement).click();
 
     return  new ProductPage(driver);
     }
